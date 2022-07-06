77b50cc0511b1b02f9a6cc8423bda5ca2b50d9b6
Juan Tapia <juant72@gmail.com>
2022-07-06 06:23:51 -0300

    Hashing blocks

--- bir/src/main.rs
+++ bir/src/main.rs
@@ -1,3 +1,21 @@
+use blockchainlib::*;
+
 fn main() {
-    println!("Blockchain in rust!");
+    let mut block = Block::new(
+        0, 
+        0,  
+        vec![0;32],
+        0,
+        "Genesis block!".to_owned()
+    );
+
+    print!("{:?}", &block);
+
+    let h= block.hash();
+    println!("{:?}", &h);
+
+    block.hash=h;
+    print!("{:?}", &block);
+
+
 }
